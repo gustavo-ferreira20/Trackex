3bd5b2c7087c31d70a2801a53116b2353c2e5392
Gustavo Rodrigues <ferreirasouza20@hotmail.com>
2022-01-03 11:48:53 +0800

    Expenses screen layout

--- Trackex/ExpensesViewController.swift
+++ Trackex/ExpensesViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class ExpensesViewController: UIViewController {
 
+    
+    @IBOutlet weak var chartView: UIView!
+    
+    @IBOutlet weak var foodExpenseButton: UIButton!
+    @IBOutlet weak var homeExpenseButton: UIButton!
+    @IBOutlet weak var billExpenseButton: UIButton!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layoutDesign()
         print("Expenses from viewDidLoad")
     }
     
@@ -20,5 +31,13 @@ class ExpensesViewController: UIViewController {
       self.tabBarController?.navigationItem.title = "Expenses"
     }
 
+    
+    private func layoutDesign(){
+        chartView.layer.cornerRadius = 5
+        addButton.layer.cornerRadius = addButton.bounds.size.height / 2
+        foodExpenseButton.layer.cornerRadius = 5
+        homeExpenseButton.layer.cornerRadius = 5
+        billExpenseButton.layer.cornerRadius = 5
+    }
 
 }
